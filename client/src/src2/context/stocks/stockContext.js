4c59d59a7967c4f994fae65995b64e388371f957
ericjchung <ericjchung@protonmail.com>
2019-07-30 06:19:18 +0000

    Removed reference files

--- client/src/src2/context/stocks/stockContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react'
-const stockContext = createContext();
-export default stockContext
--- client/src/src2/context/type.js
+++
@@ -1,5 +0,0 @@
-export const SEARCH_KEYWORD = 'SEARCH_KEYWORD'
-export const CLEAR_SEARCH = 'CLEAR_SEARCH'
-export const SET_LOADING = "SET_LOADING"
-export const SET_ALERT = "SET_ALERT"
-export const CLEAR_ALERT = "CLEAR_ALERT"
