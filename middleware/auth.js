f8f9100de605e8f20f2eb9683cb3a2eda4dd8a47
ericjchung <ericjchung@protonmail.com>
2019-08-02 04:31:18 +0000

    Restored previous authentication function

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,6 +9,8 @@ module.exports = function(req,res,next) {
         if(!token) {
         return res.status(401).json({ msg: 'No token, authorization denied'})
         }
+        const decoded = jwt.verify(token, config.get('jwtSecret'))
+        req.user = decoded.user
         next()
     } catch (err) {
         res.status(401).json( {msg: 'Invalid Token'})
