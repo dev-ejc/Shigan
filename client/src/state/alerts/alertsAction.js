790f7eb640ac04492ddc8a932ab2af74e4157326
ericjchung <ericjchung@protonmail.com>
2019-08-06 00:59:33 +0000

    Transitioning from context api to redux state management

---
+++ client/src/state/alerts/alertsAction.js
@@ -0,0 +1,15 @@
+import {SET_ALERT, REMOVE_ALERT} from './types'
+
+// Set Alert
+export const setAlert = (msg,type) => dispatch => {
+        const id = state.length + 1
+        dispatch({
+            type: SET_ALERT,
+            payload: {
+                msg,type,id
+            }
+        })
+        setTimeout(() => {
+            dispatch({type: REMOVE_ALERT,payload:id})
+        }, 5000)
+    }
