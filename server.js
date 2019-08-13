1d16cabb1223848f95c18a11c1ae38797f6e0ebe
ericjchung <ericjchung@protonmail.com>
2019-08-13 23:47:46 +0000

    Tweaked parameters for deployment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
-const { connectDB, disconnectDB } = require('./config/db')
-// const path = require('path')
+const { connectDB } = require('./config/db')
+const path = require('path')
 const passport = require('passport')
 const session = require('express-session')
-const flash = require('connect-flash')
+// const flash = require('connect-flash')
 const helmet = require('helmet')
 require('./config/passport')(passport)
 
@@ -46,14 +46,14 @@ app.use(passport.session());
 
 //@ TODO Validate parameters
 // Connect Flash
-app.use(flash());
+// app.use(flash());
 
 // Global vars
-app.use((req,res,next) => {
-    res.locals.success_msg = req.flash('success-_msg')
-    res.locals.error_msg = req.flash('success-_msg')
-    next();
-})
+// app.use((req,res,next) => {
+//     res.locals.success_msg = req.flash('success-_msg')
+//     res.locals.error_msg = req.flash('success-_msg')
+//     next();
+// })
 
 //Setting the port
 app.set("port",PORT)
@@ -72,7 +72,7 @@ var server = app.listen(app.get('port'), () => {
 app.use("/api/users",require('./routes/users'))
 app.use("/api/auth",require('./routes/auth'))
 app.use("/api/stocks",require('./routes/stocks'))
-app.use("/api/news",require('./routes/news'))
-app.use("/api/fundamentals",require('./routes/fundamentals'))
-app.use("/api/prices",require('./routes/prices'))
+//app.use("/api/news",require('./routes/news'))
+//app.use("/api/fundamentals",require('./routes/fundamentals'))
+//app.use("/api/prices",require('./routes/prices'))
 app.use("/api/portfolios",require('./routes/portfolios'))
