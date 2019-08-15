f0daceb76a1b06a65051515d6128ac1ab41a73bd
ericjchung <ericjchung@protonmail.com>
2019-08-15 12:35:28 +0000

    Additional Deployment settings

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ var sess = {
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
-    app.set('trust proxy', 1)
-    sess.cookie.secure = true 
+    //app.set('trust proxy', 1)
+    //sess.cookie.secure = true 
     app.use(express.static('client/build'))
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
 }
