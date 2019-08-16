f5d8155154ee329b8264743f0f3233e8b22e2e26
ericjchung <ericjchung@protonmail.com>
2019-08-16 07:31:27 +0000

    Port settings

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
 }
 
-app.listen(app.get('port'), () => {
+app.listen(PORT, () => {
     console.log(`Server Started`)
     connectDB()
     // setTimeout(() => {
